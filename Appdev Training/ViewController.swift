dd98ad2a99ae2dc4720ef1d6249a732f79538638
quang1610 <nguyenqu2@grinnell.edu>
2019-09-25 00:53:06 -0500

    Initial Commit

---
+++ Appdev Training/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Appdev Training
+//
+//  Created by Nguyễn Đức Quang on 9/25/19.
+//  Copyright © 2019 Nguyễn Đức Quang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
